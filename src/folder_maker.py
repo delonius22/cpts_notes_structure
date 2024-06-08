71b664e18d0db51c86791f919b545b0d25c618b3
deangelofloyd <deangelofloyd3@gmail.com>
2024-06-08 10:02:53 -0400

    cleaned up final records

--- src/folder_maker.py
+++ src/folder_maker.py
@@ -1,7 +1,9 @@
 import os,sys
 
 
-"""Making the sublime flow for new  boxes and the obsidian flow"""
+"""Making the obsidian flow for new machines"""
+
+
 def note_maker(box,dir,box_ip):
     # testing to make sure the box is not new 
     new_box = os.path.join(dir,box)
@@ -9,53 +11,34 @@ def note_maker(box,dir,box_ip):
         os.makedirs(new_box)
     except FileExistsError:
         print(f"Box {box} already exists")
-        return  
-    # making the box in the subl vault
-    if 'sublime' in new_box:
-        for val in ['enum','exploit','loot','privesc']:
-            if not os.path.exists(os.path.join(new_box,val)):
-                os.makedirs(os.path.join(new_box,val))
-            else:
-                print(f"Box {box} already exists")
-        enum_dir = os.path.join(new_box,'enum')
-        if not os.path.exists(os.path.join(enum_dir,'results')):
-            results = os.path.join(enum_dir,'results')
-            os.makedirs(os.path.join(results,box_ip))
-            box_ip_dir = os.path.join(results,box_ip)
-            for val in ['exploits','loot','scans']:
-                if not os.path.exists(os.path.join(box_ip_dir,val)):
-                    os.makedirs(os.path.join(box_ip_dir,val))
-            scan_dir = os.path.join(box_ip_dir,'scans')
-            if not os.path.exists(os.path.join(scan_dir,'active')):
-                os.makedirs(os.path.join(scan_dir,'active'))
-            if not os.path.exists(os.path.join(scan_dir,'passive')):
-                os.makedirs(os.path.join(scan_dir,'passive'))
-    elif 'obsidian' in new_box:
-        for vals in ['enum','exploit','loot','privesc','Things Ive Learned']:
-            if not os.path.exists(os.path.join(new_box,vals)):
-                os.makedirs(os.path.join(new_box,vals))
-            else:
-                print(f"Box {box} already exists")
-        enum_dir = os.path.join(new_box,'enum')
-        if not os.path.exists(os.path.join(enum_dir,'results')):
-            results = os.path.join(enum_dir,'results')
-            os.makedirs(os.path.join(results,box_ip))
-            box_ip_dir = os.path.join(results,box_ip)
-            for val in ['exploits','loot','scans']:
-                if not os.path.exists(os.path.join(box_ip_dir,val)):
-                    os.makedirs(os.path.join(box_ip_dir,val))
-            scan_dir = os.path.join(box_ip_dir,'scans')
-            if not os.path.exists(os.path.join(scan_dir,'active')):
-                os.makedirs(os.path.join(scan_dir,'active'))
-            if not os.path.exists(os.path.join(scan_dir,'passive')):
-                os.makedirs(os.path.join(scan_dir,'passive'))    
-    else:
-        print("Please specify the box type")
+        return
     
+    for val in ['enum','exploit','loot','privesc','Things Ive Learned']:
+        if not os.path.exists(os.path.join(new_box,val)):
+            os.makedirs(os.path.join(new_box,val))
+        else:
+            print(f"Box {box} already exists")
+    enum_dir = os.path.join(new_box,'enum')
+    if not os.path.exists(os.path.join(enum_dir,'results')):
+        results = os.path.join(enum_dir,'results')
+        os.makedirs(os.path.join(results,box_ip))
+        box_ip_dir = os.path.join(results,box_ip)
+        for val in ['exploits','loot','scans']:
+            if not os.path.exists(os.path.join(box_ip_dir,val)):
+                os.makedirs(os.path.join(box_ip_dir,val))
+        scan_dir = os.path.join(box_ip_dir,'scans')
+        if not os.path.exists(os.path.join(scan_dir,'active')):
+            os.makedirs(os.path.join(scan_dir,'active'))
+        if not os.path.exists(os.path.join(scan_dir,'passive')):
+            os.makedirs(os.path.join(scan_dir,'passive'))
+    else:
+        print(f"Box {os.path.join(enum_dir,'results')} already exists")
 
 if __name__ == '__main__':
-    dirs=['/Users/deangelofloyd/Documents/sublime_data','/Users/deangelofloyd/Documents/obsidian_files/HTB_Machines']
+    main_doc = '{your main directory here}'
+    dirs=['VulnHub','HTB']
     user_box = input("Enter the box name: ")
     user_box_ip = input("Enter the box ip, if you dont know it enter 0.0.0.0: ")
     for dir in dirs:
-        note_maker(user_box,dir,user_box_ip)
+        working_dir = os.path.join(main_doc,dir)
+        note_maker(user_box,working_dir,user_box_ip)
